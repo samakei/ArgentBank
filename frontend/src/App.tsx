4e077356d12ac43afa2e03d2d39dc25ed12965ab
SAMAKE Issa <samakeissa10@outlook.fr>
2024-06-05 18:13:33 +0200

    mon premier commmit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Home from './pages/Home'
+import SiGnIn from './pages/SiGnIn'
+import Footer from './components/Footer';
+import './styles/main.css';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<><Hero /><Features /></>} />
+            {/* Ajouter d'autres routes si nécessaire */}
+             <Route path="/" element={<Home />} />
+             <Route path="/sign-in" element={<SiGnIn />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+
+import { Link } from "react-router-dom";
+import logo from "../assets/argentBankLogo.webp";
+
+const Navbar = () => {
+  return (
+    <nav className="main-nav">
+      <Link className="main-nav-logo" to="/">
+        <img
+          className="main-nav-logo-image"
+          src={logo}
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      <div>
+        <Link className="main-nav-item" to="/sign-in">
+          <i className="fa fa-user-circle"></i>
+          Sign In
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
